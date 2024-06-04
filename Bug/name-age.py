2614d9789f39fd7e6d2c29453d80bb31ae33d0c3
chperette <chperette@gmail.com>
2024-06-04 14:46:28 +0200

    fix: :bug: fixed syntax error
    added missing + for concatenation, casted some variables types
    
    resolving issue #420

--- Bug/name-age.py
+++ Bug/name-age.py
@@ -1,12 +1,15 @@
 # Ce programme demande à l'utilisateur son nom et son âge, puis affiche un message de bienvenue
+try:
 
-nom = input("Entrez votre nom: ")
-age = input("Entrez votre âge: ")
+    nom = str (input("Entrez votre nom: "))
+    age = int (input("Entrez votre âge (un nombre entier): "))
 
-# La fonction print contient une erreur de syntaxe
-print("Bonjour, " + nom " ! Vous avez " + age + " ans.")
+    # La fonction print contient une erreur de syntaxe
+    print("Bonjour, " + nom + " ! Vous avez " + str (age) + " ans.")
 
-# Une petite erreur de logique ici, l'âge doit être converti en entier pour ajouter 10 ans
-age_plus_10 = age + 10
+    # Une petite erreur de logique ici, l'âge doit être converti en entier pour ajouter 10 ans
+    age_plus_10 = age + 10
 
-print("Dans 10 ans, vous aurez " + str(age_plus_10) + " ans.")
+    print("Dans 10 ans, vous aurez " + str(age_plus_10) + " ans.")
+except ValueError:
+    print ("value error")
