87c514bdd4f96656331528f040ed0c198f156d2a
wamAtam <ouahmed.automate@yahoo.fr>
2024-06-04 14:55:53 +0200

    fix: :bug: fix bug code python

--- Bug/name-age.py
+++ Bug/name-age.py
@@ -4,9 +4,9 @@ nom = input("Entrez votre nom: ")
 age = input("Entrez votre âge: ")
 
 # La fonction print contient une erreur de syntaxe
-print("Bonjour, " + nom " ! Vous avez " + age + " ans.")
+print("Bonjour, " + nom + " ! Vous avez " + age + " ans.")
 
 # Une petite erreur de logique ici, l'âge doit être converti en entier pour ajouter 10 ans
-age_plus_10 = age + 10
+age_plus_10 = int(age) + 10
 
 print("Dans 10 ans, vous aurez " + str(age_plus_10) + " ans.")
